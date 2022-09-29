a66681e7dc11d8a058fadf3d4b9da9042b7fb597
euclidespaim <euclidespaim@gmail.com>
2022-09-29 14:16:02 -0300

    Go orientado a objetos

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
 func main() {
-		println("Hello, World!")
+	var titular string = "Elon Musk"
+	var numeroAgencia int = 589
+	var numeroConta int = 1234567
+	var saldo float64 = 273.15
+
+	println("Titular da conta:", titular)
+	println("Agência:", numeroAgencia)
+	println("Conta:", numeroConta)
+	println("Saldo:", saldo)
+	
 }
