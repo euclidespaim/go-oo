949f9f3a1bcfa18af32544c6dae58ffe175e7dbe
euclidespaim <euclidespaim@gmail.com>
2022-10-03 16:03:23 -0300

    Criando módulos separados

--- contas/contaCorrente.go
+++ contas/contaCorrente.go
@@ -1,7 +1,9 @@
 package contas
 
+import "go-orientado/clientes"
+
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
 	Saldo         float64
--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"go-orientado/clientes"
 	"go-orientado/contas"
 )
 
 func main() {
 	contaDoElon := contas.ContaCorrente{
-		Titular:       "Elon Musk",
+		Titular: clientes.Titular{
+			Nome:      "Elon Musk",
+			CPF:       "123.456.789-10",
+			Profissao: "Programador",
+		},
 		NumeroAgencia: 321,
 		NumeroConta:   1234567,
 		Saldo:         100.21,
 	}
 
 	contaDoJeff := contas.ContaCorrente{
-		Titular:       "Jeff Bezos",
+		Titular: clientes.Titular{
+			Nome:      "Jeff Bezos",
+			CPF:       "123.456.789-11",
+			Profissao: "Front-end",
+		},
 		NumeroAgencia: 123,
 		NumeroConta:   7654321,
 		Saldo:         100.21,
