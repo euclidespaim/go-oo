98b5a10649dabaf0229b421bdbc0111444b06ddf
euclidespaim <euclidespaim@gmail.com>
2022-10-03 15:35:40 -0300

    Criando módulos separados

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-orientado
+
+go 1.19
--- main.go
+++ main.go
@@ -1,55 +1,29 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso \n"
-	} else {
-		return "Saldo insuficiente \n"
-	}
-}
-
-func (c*ContaCorrente) Depositar (valorDoDeposito float64) string {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Depósito realizado com sucesso \n"
-	} else {
-		return "Valor do depósito menor que zero \n"
-	}
-}
+import (
+	"fmt"
+	"go-orientado/contas"
+)
 
 func main() {
-	contaDoElon := ContaCorrente{
-		titular:       "Elon Musk",
-		numeroAgencia: 321,
-		numeroConta:   1234567,
-		saldo:         100.21,
+	contaDoElon := contas.ContaCorrente{
+		Titular:       "Elon Musk",
+		NumeroAgencia: 321,
+		NumeroConta:   1234567,
+		Saldo:         100.21,
 	}
-	fmt.Println(contaDoElon.Sacar(100))
-	fmt.Println("Saldo na conta do titular: ", contaDoElon.titular, "= ", contaDoElon.saldo)
-	fmt.Println()
 
-	contaDoElon.Depositar(300)
-	fmt.Println("Saldo na conta do titular: ", contaDoElon.titular, "= ", contaDoElon.saldo)
+	contaDoJeff := contas.ContaCorrente{
+		Titular:       "Jeff Bezos",
+		NumeroAgencia: 123,
+		NumeroConta:   7654321,
+		Saldo:         100.21,
+	}
 
-	contaDoBezos := ContaCorrente{"Jeff Bezos", 123, 1234567, 200.00}
-	fmt.Println("Saldo na conta do titular: ", contaDoBezos.titular, "= ", contaDoBezos.saldo)
-	fmt.Println()
+	status := contaDoElon.Transferir(100, &contaDoJeff)
 
-	var contaDoStark *ContaCorrente
-	contaDoStark = new(ContaCorrente)
-	contaDoStark.titular = "Tony Stark"
-	contaDoStark.numeroAgencia = 123
-	fmt.Println(*contaDoStark)
-	fmt.Println()
+	fmt.Println(status)
+	fmt.Println(contaDoJeff)
+	fmt.Println(contaDoElon)
+
 }
